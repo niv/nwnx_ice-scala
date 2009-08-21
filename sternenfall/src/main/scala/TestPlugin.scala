f19ffedc5dd388aa085fe61b4161f3a679452b47
Bernhard Stoeckner <elven@swordcoast.net>
2009-08-21 20:11:04 +0200

    initial import: Sternenfall code

---
+++ sternenfall/src/main/scala/TestPlugin.scala
@@ -0,0 +1,12 @@
+import NWN._
+import es.elv.kobold._
+import es.elv.kobold.Implicits._
+import net.lag._
+
+class TestPlugin extends Plugin {
+	private val log = logging.Logger.get
+
+	def listen(event: Event) {
+
+	}
+}
