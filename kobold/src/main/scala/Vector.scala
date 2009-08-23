640546644f36d9019ea920a27fb5f5dbf1630092
Bernhard Stoeckner <elven@swordcoast.net>
2009-08-23 19:50:41 +0200

    doc: Vector

--- kobold/src/main/scala/Vector.scala
+++ kobold/src/main/scala/Vector.scala
@@ -5,32 +5,47 @@ package es.elv.kobold {
 		val origin = Vector(0, 0, 0)
 	}
 
+	/**
+		A Vector represents a point in 3d-space. This is equivalent
+		to the nwscript "vector" datatype.
+	*/
 	case class Vector(val x: Double, val y: Double, val z: Double) {
 		require(x >= 0.0)
 		require(y >= 0.0)
 
+		/** The angle of this Vector. */
 		lazy val angle = this match {
 			case Vector(0, 0, 0) => 0
 			case _ => angleTo(Vector(0, 0, 0))
 		}
 
+		/** The magnitude of this Vector. */
 		lazy val magnitude = distanceTo(Vector(0, 0, 0))
 
+		/** Calculates the distance from this Vector to to another Vector. */
 		def distanceTo(lo: Vector) =
 			Math.sqrt(Math.pow(lo.x - x, 2) + Math.pow(lo.y - y, 2) + Math.pow(lo.z - z, 2))
 		
+		/** Creates a new Vector at the position distance units away at the given angle. */
 		def positionFrom(distance: Double, angle: Double) =
 			new Vector(Math.abs(x + distance * Math.cos(angle)), Math.abs(y + distance * Math.sin(angle)), z)
 
+		/** Calculates the angle to another Vector. */
 		def angleTo(lo: Vector) =
 			Math.acos(Math.abs((x - lo.x).toInt).toDouble / distanceTo(lo))
 
+		/** Returns the normalised unit-Vector. */
 		def normalize = this / magnitude
 
+		/** Add another vector to this one, returning a new Vector. */
 		def +(o: Vector) = Vector(o.x + x, o.y + y, o.z + z)
+		/** Substract another vector from this one, returning a new Vector. */
 		def -(o: Vector) = Vector(x - o.x, y - o.y, z - o.z)
+		/** Multiply another vector with this one, returning a new Vector. */
 		def *(o: Double) = Vector(x * o, y * o, z * o)
+		/** Divide this Vector with another one, returning a new Vector. */
 		def /(o: Double) = Vector(x / o, y / o, z / o)
+		/** Create the dot product between this Vector and the given one. */
 		def dot(o: Vector) = x * o.x + y * o.y + z * o.z
 	}
 }
