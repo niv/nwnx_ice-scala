df661ce160efdd99b5f3cc0a8f64edc1266b30a5
Bernhard Stoeckner <elven@swordcoast.net>
2009-08-26 23:45:46 +0200

    some NWNX stubs

--- kobold/src/main/scala/NWNX.scala
+++ kobold/src/main/scala/NWNX.scala
@@ -5,17 +5,55 @@ package es.elv.kobold.nwnx {
 	import Implicits._
 
 	object Core {
-		def get(obj: NWObject, lib: String, key: String, buf: Int) = {
+		def set(obj: NWObject, lib: String, key: String, value: String) =
+			R.proxy.setLocalString(obj, "NWNX!" + lib + "!" + key, value)
+		def get(obj: NWObject, lib: String, key: String, buf: Int) =
+			setGet(obj, lib, key, " " * buf)
+
+		def getInt(obj: NWObject, lib: String, key: String) =
+			get(obj, lib, key, 12).toInt
+
+		def setGet(obj: NWObject, lib: String, key: String, init: String) = {
 			R.proxy.setLocalString(obj, "NWNX!" + lib + "!" + key,
-				" " * buf)
+				init)
 			R.proxy.getLocalString(obj, "NWNX!" + lib + "!" + key)
 		}
+
 		def getObject(obj: NWObject, lib: String, key: String) =
 			R.proxy.getLocalObject(obj, "NWNX!" + lib + "!" + key)
 
 	}
 
-	object Chat {
+	abstract class Core(val nwnxKey: String) {
+		def get(obj: NWObject, key: String, buf: Int) =
+			Core.setGet(obj, nwnxKey, key, " " * buf)
+		def set(obj: NWObject, key: String, value: String) =
+			Core.set(obj, nwnxKey, key, value)
+		def getObject(obj: NWObject, key: String) =
+			Core.getObject(obj, nwnxKey, key)
+	}
+
+	object Funcs extends Core("FUNCS") {
+		def getInt(obj: NWObject, key: String, args: Int*) =
+			Core.setGet(obj, "FUNCS", key, args map(_ toString) mkString(" ") + " " * 10).toInt
+
+		def allAreas: List[Area] = {
+			var all = List[Area]()
+			var area = Area(getObject(Module(), "GETFIRSTAREA"))
+			while (area.valid) {
+				area = Area(getObject(Module(), "GETNEXTAREA"))
+				all ::= area
+			}
+			all
+		}
+	}
+
+	object Structs extends Core("STRUCTS") {
+		def setEffectSpellId(obj: NWObject, effect: Effect, spell: Int) =
+			set(Module(), "SETSPELLID", spell.toString)
+	}
+
+	object Chat extends Core("CHAT") {
 		def speakToChannel(speaker: GameObject[_], channel: Int, text: String, to: GameObject[_]): Unit = {
 			if (!speaker.valid)
 				return
@@ -27,11 +65,11 @@ package es.elv.kobold.nwnx {
 				to.wrapped.id.toHexString, channel, text).mkString("¬")
 
 			// R.proxy.setLocalInt(speaker, "ice_chat_message_ignore", 1)
-			R.proxy.setLocalString(speaker, "NWNX!CHAT!SPEAK", t)
+			set(speaker, "SPEAK", t)
 		}
 
 		def pcIn(o: Player) {
-			val id = Core.get(o, "CHAT", "GETID", 10).trim.toInt
+			val id = get(o, "GETID", 10).trim.toInt
 			if (id != -1) {
 				R.proxy.setLocalObject(Module(), "chatPC_" + id, o)
 				R.proxy.setLocalInt(o, "chatID", id)
@@ -42,6 +80,7 @@ package es.elv.kobold.nwnx {
 			R.proxy.deleteLocalInt(o, "chatID")
 			R.proxy.deleteLocalObject(Module(), "chatPC_" + id)
 		}
+
 		def getPC(id: Int): Player = {
 			Player(R.proxy.getLocalObject(Module(), "chatPC_" + id))
 		}
