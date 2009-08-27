dd26cc6625a6079b48b215dc886b2972c27a866e
Bernhard Stoeckner <elven@swordcoast.net>
2009-08-27 12:15:47 +0200

    Project.scala: update sub-projects to have proper dep tracking

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,9 +2,20 @@ import sbt._
 
 
 class KoboldProject(info: ProjectInfo) extends ParentProject(info) {
-	val scalatest = "org.scala-tools.testing" % "scalatest" % "0.9.5"
+	override def parallelExecution = true
 
-	lazy val nwnxice = project("nwnxice", "NWNXICE glue code")
-	lazy val kobold = project("kobold", "Kobold Core", nwnxice)
-	lazy val sternenfall = project("sternenfall", "Sternenfall", nwnxice, kobold)
+	lazy val nwnxice = project("nwnxice", "NWNXICE java bindings", new NWNXICE(_))
+	lazy val kobold = project("kobold", "Kobold core", new Kobold(_), nwnxice)
+	lazy val sternenfall = project("sternenfall", "Sternenfall", new Sternenfall(_), nwnxice, kobold)
+
+	class Kobold(info: ProjectInfo) extends DefaultProject(info) {
+		val scalatest = "org.scala-tools.testing" % "scalatest" % "0.9.5"
+		val configgy = "net.lag" % "configgy" % "1.4" from "http://www.lag.net/repo/net/lag/configgy/1.4/configgy-1.4.jar"
+	}
+
+	class NWNXICE(info: ProjectInfo) extends DefaultProject(info) {
+	}
+
+	class Sternenfall(info: ProjectInfo) extends DefaultProject(info) {
+	}
 }
