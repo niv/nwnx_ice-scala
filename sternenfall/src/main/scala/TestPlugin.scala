6efad25e142abefc5d714252a538d259d45d3c04
Bernhard Stoeckner <elven@swordcoast.net>
2009-09-01 16:18:06 +0200

    TestPlugin example: update

--- sternenfall/src/main/scala/TestPlugin.scala
+++ sternenfall/src/main/scala/TestPlugin.scala
@@ -1,12 +1,10 @@
 import NWN._
 import es.elv.kobold._
+import es.elv.kobold.events._
 import es.elv.kobold.Implicits._
-import net.lag._
 
 class TestPlugin extends Plugin {
-	private val log = logging.Logger.get
-
-	def listen(event: Event) {
-
+	def listen(event: Event) = event match {
+		case _ =>
 	}
 }
