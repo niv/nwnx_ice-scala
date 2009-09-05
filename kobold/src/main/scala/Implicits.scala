7f807736b883c7deb03902f5c12c8929df22aec2
Bernhard Stoeckner <elven@swordcoast.net>
2009-09-05 18:33:54 +0200

    Implicits: fix StackOverflow in Location

--- kobold/src/main/scala/Implicits.scala
+++ kobold/src/main/scala/Implicits.scala
@@ -6,11 +6,11 @@ package es.elv.kobold {
 		implicit def iprpj2nwiprp(n: ItemProperty): NWItemProperty = n.toNWItemProperty
 		implicit def go2nwobj(n: G): NWObject = n.wrapped
 
-		implicit def loc2nwloc(l: Location): NWLocation = new NWLocation(l.area.wrapped, l.x, l.y, l.z, l.facing, 0f, 0f)
-		implicit def nwloc2loc(l: NWLocation): Location = new Location(new Area(l.area), l.x, l.y, l.z, l.orientationX)
-		implicit def vec2nwvec(l: Vector): NWVector = new NWVector(l.x, l.y, l.z)
+		implicit def loc2nwloc(l: Location): NWLocation = new NWLocation(l.area.wrapped, l.position.x, l.position.y, l.position.z, l.facing, 0f, 0f)
+		implicit def nwloc2loc(l: NWLocation): Location = new Location(G[Area](l.area), l.x, l.y, l.z, l.orientationX)
 		implicit def nwloc2loc(l: NWVector): Vector = new Vector(l.x, l.y, l.z)
+		implicit def vec2nwvec(l: Vector): NWVector = new NWVector(l.x, l.y, l.z)
 
-		implicit def ivec2vec(v: (Double, Double, Double)) = new Vector(v._1, v._2, v._3)
+		// implicit def ivec2vec(v: (Double, Double, Double)) = Vector(v._1, v._2, v._3)
 	}
 }
