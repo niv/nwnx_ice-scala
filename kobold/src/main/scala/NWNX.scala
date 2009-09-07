88681cacb065bfac6292c527699a25f34bc7f686
Bernhard Stoeckner <elven@swordcoast.net>
2009-09-07 10:35:11 +0200

    NWNX: revfactor

--- kobold/src/main/scala/NWNX.scala
+++ kobold/src/main/scala/NWNX.scala
@@ -3,23 +3,16 @@ package es.elv.kobold.nwnx {
 	import Implicits._
 
 	object Core {
-		def set(obj: NWObject, lib: String, key: String, value: String) =
-			R.proxy.setLocalString(obj, "NWNX!" + lib + "!" + key, value)
 		def get(obj: NWObject, lib: String, key: String, buf: Int) =
 			setGet(obj, lib, key, " " * buf)
-
-		def getInt(obj: NWObject, lib: String, key: String) =
-			get(obj, lib, key, 12).toInt
-
+		def set(obj: NWObject, lib: String, key: String, value: String) =
+			R.proxy.setLocalString(obj, "NWNX!" + lib + "!" + key, value)
 		def setGet(obj: NWObject, lib: String, key: String, init: String) = {
-			R.proxy.setLocalString(obj, "NWNX!" + lib + "!" + key,
-				init)
+			R.proxy.setLocalString(obj, "NWNX!" + lib + "!" + key, init)
 			R.proxy.getLocalString(obj, "NWNX!" + lib + "!" + key)
 		}
-
 		def getObject(obj: NWObject, lib: String, key: String) =
 			R.proxy.getLocalObject(obj, "NWNX!" + lib + "!" + key)
-
 	}
 
 	abstract class Core(val nwnxKey: String) {
@@ -27,6 +20,8 @@ package es.elv.kobold.nwnx {
 			Core.setGet(obj, nwnxKey, key, " " * buf)
 		def set(obj: NWObject, key: String, value: String) =
 			Core.set(obj, nwnxKey, key, value)
+		def setGet(obj: NWObject, key: String, init: String) =
+			Core.setGet(obj, nwnxKey, key, init)
 		def getObject(obj: NWObject, key: String) =
 			Core.getObject(obj, nwnxKey, key)
 	}
