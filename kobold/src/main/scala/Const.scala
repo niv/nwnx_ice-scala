42cade4b77508de0d37e7a70a9d777f9a234b475
Bernhard Stoeckner <elven@swordcoast.net>
2009-09-07 10:33:48 +0200

    add missing consts for ext/ChatListener

---
+++ kobold/src/main/scala/Const.scala
@@ -0,0 +1,9 @@
+package es.elv.kobold
+
+object SoundDistance {
+	val MeterToFeet = 3.2808399
+	val FeetToMeter = 0.3048
+
+	val WhisperVolume =  5.0  * MeterToFeet
+	val TalkVolume    =  30.0 * MeterToFeet
+}
