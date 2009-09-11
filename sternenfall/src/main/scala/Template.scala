acc4b455fdc39026dd6263f04ba31370026ec3bd
Bernhard Stoeckner <elven@swordcoast.net>
2009-09-11 13:25:35 +0200

    sf: Template: generic plugin template

---
+++ sternenfall/src/main/scala/Template.scala
@@ -0,0 +1,11 @@
+package org.sternenfall.kobold
+
+import NWN._
+import es.elv.kobold._
+import es.elv.kobold.events._
+
+object Template extends Plugin {
+	def listen(event: Event) = event match {
+		case _ =>
+	}
+}
