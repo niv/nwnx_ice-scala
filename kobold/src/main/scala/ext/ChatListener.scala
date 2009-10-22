539e1cd8d97d3b07e006b57b5f6cb6fff9026580
Bernhard Stoeckner <elven@swordcoast.net>
2009-10-22 14:27:42 +0200

    ext/ChatListener: add OnChatPartyHeard for PlayerCreatures

--- kobold/src/main/scala/ext/ChatListener.scala
+++ kobold/src/main/scala/ext/ChatListener.scala
@@ -8,13 +8,22 @@ package events {
 
 /**
 	Fires for each Creature that hears another Creature talking.
+	@see ext.ChatListener
 */
 case class OnChatTalkHeard(talker: Creature, listener: Creature, text: String) extends GameEvent
 
 /**
 	Fires for each Creature that hears another Creature whispering.
+	@see ext.ChatListener
 */
 case class OnChatWhisperHeard(talker: Creature, listener: Creature, text: String) extends GameEvent
+
+/**
+	Fires for each Creature that hears another Creature speaking on the party channel.
+	Triggers only for players, not for NPCs.
+	@see ext.ChatListener
+*/
+case class OnChatPartyHeard(talker: PlayerCreature, listener: Creature, text: String) extends GameEvent
 
 }
 
@@ -23,18 +32,23 @@ package ext {
 /**
 	Listens for Chat events and distributes Creature-targeted specific events.
 	Overhead should be minor.
+	@see events.OnChatTalkHeard, events.OnChatWhisperHeard, events.OnChatPartyHeard
 */
 object ChatListener extends Plugin {
 	def listen(event: Event) = event match {
 
 		case OnChatTalk(talker: Creature, text: String) => {
 			talker.near(SoundDistance.TalkVolume, NWN.ObjectType.Creature, classOf[Creature]) filter (talker hears _) foreach (listener =>
-				if (listener.alive) EventSource send OnChatTalkHeard(talker, listener, text))
+				EventSource send OnChatTalkHeard(talker, listener, text))
 		}
 		
 		case OnChatWhisper(talker: Creature, text: String) =>
 			talker.near(SoundDistance.WhisperVolume, NWN.ObjectType.Creature, classOf[Creature]) filter (talker hears _) foreach (listener =>
-				if (listener.alive) EventSource send OnChatWhisperHeard(talker, listener, text))
+				EventSource send OnChatWhisperHeard(talker, listener, text))
+
+		case OnChatParty(talker: PlayerCreature, text: String) =>
+			talker.faction().members() filter (_ != talker) foreach (listener =>
+				EventSource send OnChatPartyHeard(talker, listener, text))
 
 		case _ =>
 	}
